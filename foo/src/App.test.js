fab22f4f1863179430531ce4eedb8dbaf62d4cab
Lucas Polito <lucas@outschool.com>
2021-05-20 15:36:45 -0400

    Make a failure

--- foo/src/App.test.js
+++ foo/src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('failure', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn bupkis/i);
+  expect(linkElement).toBeInTheDocument();
+});
