7667bba5ac106cca39f6b640b4991ae7cf0cb0a1
Lucas Polito <lucas@outschool.com>
2021-05-24 13:29:40 -0400

    these docs suck

--- foo/.jest-circus-allure-base-environment.js
+++ foo/.jest-circus-allure-base-environment.js
@@ -29,7 +29,7 @@ module.exports = (classExtend) => class extends classExtend {
         await super.setup();
 
         this.allure = new AllureRuntime({
-            resultsDir: 'allure-report'
+            resultsDir: 'allure-results'
         });
         this.global.allureRuntime = this.allure;
     }
