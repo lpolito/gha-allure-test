598d0429e4ab5382b794c9d0de76ecc79f221b7c
Lucas Polito <lucas@outschool.com>
2021-05-26 11:49:25 -0400

    More tests

--- foo/src/App2.test.js
+++ foo/src/App2.test.js
@@ -1,26 +1,27 @@
 import { render, screen } from '@testing-library/react';
+import { describe } from 'jest-circus';
 import App from './App';
 
-test('renders learn react link', () => {
+test('2 renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders but doesn\'t exist', () => {
+test('2 renders but doesn\'t exist', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn bupkis/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-describe('we are testing this thing but it\'s nested', () => {
-  test('nested renders learn react link', () => {
+describe('2 we are testing this thing but it\'s nested', () => {
+  test('2 nested renders learn react link', () => {
     render(<App />);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
   });
   
-  test('nested renders but doesn\'t exist', () => {
+  test('2 nested renders but doesn\'t exist', () => {
     render(<App />);
     const linkElement = screen.getByText(/learn bupkis/i);
     expect(linkElement).toBeInTheDocument();
